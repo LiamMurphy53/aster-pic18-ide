45fb1d5f4546a0dad672c0f8675304e7b02bcf5d
LiamMurphy53 <murli01quebo@gmail.com>
2026-09-17 13:23:19 -0600

    Add Examples/FirstLight.X/nbproject files

---
+++ Examples/FirstLight.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,10 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=FirstLight.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/FirstLight.X.production.hex
